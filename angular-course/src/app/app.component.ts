f1cb034b8d4c61096b62a8acad2605c1fbb10fe4
Eric Ocana <eocana.joviat@gmail.com>
2022-08-10 13:15:05 +0200

    new section angular components

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data = {
-    title: "<h1>my angular course</h1>",
-    msgInput: "Lore Ipsum"
-  };
 
-  onLogoClicked () {
-    alert('Hello world!');
-  }
 
-  onKeyUp(newTitle: string) {
-    this.data.title = newTitle;
-  }
+
 }
