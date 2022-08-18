35021455817c6155fed58be3794877bdacc2629e
Eric Ocana <eocana.joviat@gmail.com>
2022-08-18 20:23:54 +0200

    *Use of ViewChild and ngAfterViewInit

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -7,9 +8,64 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
+  startDate = new Date();
+
+
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards: QueryList<ElementRef>;
+
+
+
+  /*@ViewChild('container')
+  containerDiv: ElementRef;*/
+
+
+
+  constructor(){
+
+
+  }
+  ngAfterViewInit() {
+    //console.log('AfterInit containerDiv: ', this.containerDiv);
+      // this.cards.changes.subscribe(
+      //   cards => console.log(cards)
+      // );
+    
+    console.log(this.cards)
+    
+  }
+
+  onCoursesEdited(){
+    this.courses.push(
+      {
+        id: 11,
+        description: "Collei",
+        longDescription: "A trainee ranger active in Avidya Forest. Behind her enthusiastic words and actions hides a slightly introverted personality.",
+        iconUrl: '../assets/collei.png',
+      }
+    );
+  }
+
+  oncourseSelected(course: Course) {
+
+    
+
+  }
+}
+
+
+
+
+  /**ViewChild with AfterViewInit
+  @ViewChild('cardRef1', {read: ElementRef})
+  card1: CourseCardComponent;
+
+  @ViewChild('cardRef2')
+  card2: CourseCardComponent; */
+
 
  /*  
   Without ng for core directive
@@ -19,13 +75,9 @@ export class AppComponent {
 
   ngrxCourse = COURSES[2]; */
 
-  title = COURSES[0].description;
-  startDate = new Date();
+/* 
   level = 90.00;
   CRITrate = 0.5345;
   course = COURSES[0];
-
-  oncourseSelected(course: Course) {
-    console.log('Card clicked by: card-component', course);
-  }
-}
+  title = COURSES[0].description;
+*/
